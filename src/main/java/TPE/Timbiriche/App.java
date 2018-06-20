c22dda83d32479ff40b6a02d89df0ad691f6aa0f
tdorado <tdorado@itba.edu.ar>
2018-06-20 00:56:03 -0300

    Lines in the view are now printing well

--- src/main/java/TPE/Timbiriche/App.java
+++ src/main/java/TPE/Timbiriche/App.java
@@ -16,6 +16,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.scene.text.Text;
+
 import javax.xml.soap.Node;
 import java.awt.*;
 import java.io.IOException;
@@ -28,32 +29,31 @@ public class App extends Application {
     private Board board;
     private Coordinates cords[] = new Coordinates[2];
     private int cont;
-    private static Text points1 = new Text(600,150,"0");
-    private static Text points2 = new Text(600,175,"0");
+    private static Text points1 = new Text(600, 150, "0");
+    private static Text points2 = new Text(600, 175, "0");
 
     public static Text getPoints1() {
         return points1;
     }
-    public static Text getPoints2(){
+
+    public static Text getPoints2() {
         return points2;
     }
 
-    public static void main(String[] args )
-    {
+    public static void main(String[] args) {
         game = null;
 
         int size = 0, aiType = 0, aiMode = 0, aiModeParam = 0;
         boolean prune = false, argumentFail = false, loadFromFile = false;
         String fileName = null;
 
-        if(args.length == 4 || args.length == 6 || args.length == 10 || args.length == 12) {
+        if (args.length == 4 || args.length == 6 || args.length == 10 || args.length == 12) {
             if (args[0].equals("-size")) {
                 size = Integer.parseInt(args[1]);
                 if (size < 1 && size > 30) {
                     argumentFail = true;
                 }
-            }
-            else{
+            } else {
                 argumentFail = true;
             }
             if (args[2].equals("-ai")) {
@@ -61,21 +61,18 @@ public class App extends Application {
                 if (aiType < 0 && aiType > 3) {
                     argumentFail = true;
                 }
-            }
-            else{
+            } else {
                 argumentFail = true;
             }
-            if(args.length > 4) {
-                if(args.length == 6){
+            if (args.length > 4) {
+                if (args.length == 6) {
                     if (args[4].equals("-load")) {
                         loadFromFile = true;
                         fileName = args[5];
-                    }
-                    else{
+                    } else {
                         argumentFail = true;
                     }
-                }
-                else {
+                } else {
                     if (args[4].equals("-mode")) {
                         if (args[5].equals("time")) {
                             aiMode = 0;
@@ -108,55 +105,49 @@ public class App extends Application {
                     }
                 }
             }
-            if(args.length > 10) {
+            if (args.length > 10) {
                 if (args[10].equals("-load")) {
                     loadFromFile = true;
                     fileName = args[11];
-                }
-                else{
+                } else {
                     argumentFail = true;
                 }
             }
-        }
-        else{
+        } else {
             argumentFail = true;
         }
 
-        if(!argumentFail){
+        if (!argumentFail) {
             boolean error = false;
-            if(!loadFromFile) {
+            if (!loadFromFile) {
                 game = new Game(size, aiType, aiMode, aiModeParam, prune);
-            }
-            else{
-                try{
+            } else {
+                try {
                     game = Game.loadGameFromFile(size, aiType, aiMode, aiModeParam, prune, fileName);
-                }
-                catch (Exception e){
+                } catch (Exception e) {
                     error = true;
                     System.out.println(e);
                 }
             }
-            if(game == null){
+            if (game == null) {
                 error = true;
             }
-            if(!error) {
+            if (!error) {
                 launch(args);
-            }
-            else{
+            } else {
                 System.out.println("\nError: Wrong input parameters, please try again.");
             }
-        }
-        else{
+        } else {
             System.out.println("\nError: Wrong input parameters, please try again.");
         }
     }
 
-    private static void testDeAIPlayer(){
+    private static void testDeAIPlayer() {
         int i = 0;
-        while(!game.getGameBoard().isOver()) {
+        while (!game.getGameBoard().isOver()) {
             try {
-                ((AIPlayer)game.getCurrentPlayer()).calculateAndMakeMove();
-                System.out.println(((AIPlayer)game.getNotCurrentPlayer()).makeDotFile("player1" + i++));
+                ((AIPlayer) game.getCurrentPlayer()).calculateAndMakeMove();
+                System.out.println(((AIPlayer) game.getNotCurrentPlayer()).makeDotFile("player1" + i++));
             } catch (MinimaxException e) {
                 e.printStackTrace();
             }
@@ -219,39 +210,36 @@ public class App extends Application {
         primaryStage.show();
 
 
-        while(!game.getGameBoard().isOver()){   //aca dentro va lo del text de turno
+        while (!game.getGameBoard().isOver()) {   //aca dentro va lo del text de turno
             Player actualPlayer = game.getCurrentPlayer();
-            if(actualPlayer.isAI()){
-                ((AIPlayer)actualPlayer).calculateAndMakeMove();
-            }
-            else{
+            if (actualPlayer.isAI()) {
+                ((AIPlayer) actualPlayer).calculateAndMakeMove();
+            } else {
                 Move moveToMake = getMove();
                 actualPlayer.makeMovePlayer(moveToMake);
             }
+
+            board.refreshBoard();
         }
-
-
-
     }
 
-    private Move getMove(){
+    private Move getMove() {
         Move move = null;
         cont = 0;
-        while(move == null){
+        while (move == null) {
             scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent mouseEvent) {
-                    if(board.existMove((int)mouseEvent.getX(),(int)mouseEvent.getY())){
-                        cords[cont] = new Coordinates((int)mouseEvent.getX(),(int)mouseEvent.getY());
+                    if (board.existMove((int) mouseEvent.getX(), (int) mouseEvent.getY())) {
+                        cords[cont] = new Coordinates((int) mouseEvent.getX(), (int) mouseEvent.getY());
                         cont++;
                     }
                 }
             });
-            if(cont == 2){ // ya tengo 2 puntos
-                if(board.validMove(cords[0].x,cords[0].y,cords[1].x,cords[1].y)){ //si es un movimiento valido lo creo
-                    move = new Move(cords[0].y / 30,cords[0].x / 30,cords[1].y / 30,cords[1].x / 30);
-                }
-                else{   //preparo espera de dos puntos nuevos
+            if (cont == 2) { // ya tengo 2 puntos
+                if (board.validMove(cords[0].x, cords[0].y, cords[1].x, cords[1].y)) { //si es un movimiento valido lo creo
+                    move = new Move(cords[0].y / 30, cords[0].x / 30, cords[1].y / 30, cords[1].x / 30);
+                } else {   //preparo espera de dos puntos nuevos
                     cont = 0;
                 }
             }
@@ -261,7 +249,7 @@ public class App extends Application {
     }
 
 
-    private class Coordinates{
+    private class Coordinates {
         private int x;
         private int y;
 
--- src/main/java/TPE/Timbiriche/view/Board.java
+++ src/main/java/TPE/Timbiriche/view/Board.java
@@ -14,72 +14,72 @@ public class Board extends Pane {
 
     private static final int DISTANCE = 30;
     private static final int RADIUS = 5;
+    private static final int LINE_EXTEND = 4;
+    private static final int LINE_DISTANCE = 20;
     private static Game g;
 
-    public Board(Game g){
+    public Board(Game g) {
 
         this.g = g;
         refreshBoard();
     }
 
 
-
-    public void refreshBoard(){
+    public void refreshBoard() {
         int i;
         int j;
 
         App.getPoints1().setText("Player 1 point: " + g.getPlayer1().getPoints());
         App.getPoints2().setText("Player 2 point: " + g.getPlayer2().getPoints());
 
-        for(i=0;i<g.getGameBoard().getSize();i++){
-            for(j=0;j<g.getGameBoard().getSize();j++){
-                Circle c = new Circle(RADIUS,Color.BLACK);
-                c.relocate(i*DISTANCE,j*DISTANCE);
+        for (i = 0; i < g.getGameBoard().getSize(); i++) {
+            for (j = 0; j < g.getGameBoard().getSize(); j++) {
+                Circle c = new Circle(RADIUS, Color.BLACK);
+                c.relocate(i * DISTANCE, j * DISTANCE);
                 getChildren().add(c);
             }
         }
 
-        for(MoveDone d : g.getGameBoard().getMovesDone()){
+        for (MoveDone d : g.getGameBoard().getMovesDone()) {
             Rectangle arc = new Rectangle();
-            arc.setX(d.getMove().getRowFrom()+ 2.5);
-            arc.setY(d.getMove().getColFrom()+2.5);
+            arc.setArcWidth(1);
+            arc.setArcHeight(1);
 
-            if(d.getMove().isHorizontal()) {
+            if (d.getMove().isHorizontal()) {
+                arc.setWidth(LINE_DISTANCE);
+                arc.setHeight(LINE_EXTEND);
+                arc.setX((d.getMove().getColFrom() * DISTANCE) + RADIUS * 2);
+                arc.setY((d.getMove().getRowFrom() * DISTANCE) + (RADIUS * 2 - LINE_EXTEND) / 2);
 
-                arc.setWidth(20);
-                arc.setHeight(3);
-                arc.setArcWidth(1);
-                arc.setArcHeight(1);
+            } else {
+                arc.setWidth(LINE_EXTEND);
+                arc.setHeight(LINE_DISTANCE);
+                arc.setX((d.getMove().getColFrom() * DISTANCE) + (RADIUS * 2 - LINE_EXTEND) / 2);
+                arc.setY((d.getMove().getRowFrom() * DISTANCE) + RADIUS * 2);
+
             }
 
-            if(d.getMove().isVertical()) {
-
-                arc.setWidth(3);
-                arc.setHeight(20);
-                arc.setArcWidth(1);
-                arc.setArcHeight(1);
-            }
-
-            if(d.getPlayer() == g.getPlayer1()){
+            if (d.getPlayer() == g.getPlayer1()) {
                 arc.setFill(Color.RED);
-            }
-            else {
+            } else {
                 arc.setFill(Color.BLUE);
             }
+
             getChildren().add(arc);
         }
     }
-    public boolean existMove(int x,int y){
-        if(x >= 0 && x <= DISTANCE * g.getGameBoard().getSize()  && y >= 0 && y <= DISTANCE * g.getGameBoard().getSize() ){ //esta dentro del tablero
-            if(x % DISTANCE <= 5 && y % DISTANCE <= 5){ //esta dentro del radio de un punto
+
+    public boolean existMove(int x, int y) {
+        if (x >= 0 && x <= DISTANCE * g.getGameBoard().getSize() && y >= 0 && y <= DISTANCE * g.getGameBoard().getSize()) { //esta dentro del tablero
+            if (x % DISTANCE <= 5 && y % DISTANCE <= 5) { //esta dentro del radio de un punto
                 return true;
             }
         }
         return false;
     }
-    public boolean validMove(int x1,int y1,int x2, int y2){
-        if(Math.sqrt(Math.pow(x2 - x1 ,2) + Math.pow(y2 - y1 ,2)) <= DISTANCE + 2 * RADIUS)
-        {
+
+    public boolean validMove(int x1, int y1, int x2, int y2) {
+        if (Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)) <= DISTANCE + 2 * RADIUS) {
             return true;
         }
         return false;
